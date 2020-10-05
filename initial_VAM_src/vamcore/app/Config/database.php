fe4e0d44a86931825b752ee12888dbdadf7164b2
Unai <usarasola@stratio.com>
2020-10-05 15:09:48 +0200

    Pending templates

---
+++ initial_VAM_src/vamcore/app/Config/database.php
@@ -0,0 +1,13 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'xxxxxx',
+		'password' => 'xxxxxx',
+		'database' => 'xxxxxx',
+		'encoding' => 'utf8'
+	);
+}
